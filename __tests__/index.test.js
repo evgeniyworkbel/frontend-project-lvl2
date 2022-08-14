f389958d300737d4c9e864f96c2a411c43f0cca8
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2022-08-14 12:42:21 +0300

    Add expected result plain formatter, add tests for it

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+// Tests for stylish output format
 test('genDiff (json-json)', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('expectedStylish.txt'));
 });
@@ -24,4 +25,21 @@ test('genDiff (json-yaml)', () => {
 
 test('genDiff (json-yml)', () => {
   expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file2.json'))).toEqual(readFile('expectedStylish.txt'));
+});
+
+// Tests for plain output format
+test('genDiff (json-json)', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(readFile('expectedPlain.txt'));
+});
+
+test('genDiff (yml-yaml)', () => {
+  expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file4.yaml'), 'plain')).toEqual(readFile('expectedPlain.txt'));
+});
+
+test('genDiff (json-yaml)', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file4.yaml'), 'plain')).toEqual(readFile('expectedPlain.txt'));
+});
+
+test('genDiff (json-yml)', () => {
+  expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file2.json'), 'plain')).toEqual(readFile('expectedPlain.txt'));
 });
